833febb676ef51ac1c097202bba74c07cdc0ea92
vitin1524 <v.pereira24@escola.pr.gov.br>
2026-04-09 10:45:13 -0300

    sd

--- service-worker.js
+++ service-worker.js
@@ -2,19 +2,19 @@ const CACHE_NAME = 'lista-compras-v1';
 const urlsToCache = [
   '/',
   '/login.html',
-  '/login.css',
+  '/src/css/login.css',
   '/index.html',
-  '/style.css',
+  '/src/css/index.css',
   '/app.js',
   '/manifest.json',
-  '/icons/icon-72x72.png',
-  '/icons/icon-96x96.png',
-  '/icons/icon-128x128.png',
-  '/icons/icon-144x144.png',
-  '/icons/icon-152x152.png',
-  '/icons/icon-192x192.png',
-  '/icons/icon-384x384.png',
-  '/icons/icon-512x512.png'
+  '/src/icons/icon-72x72.png',
+  '/src/icons/icon-96x96.png',
+  '/src/icons/icon-128x128.png',
+  '/src/icons/icon-144x144.png',
+  '/src/icons/icon-152x152.png',
+  '/src/icons/icon-192x192.png',
+  '/src/icons/icon-384x384.png',
+  '/src/icons/icon-512x512.png'
 ];
 
 self.addEventListener('install', event => {
